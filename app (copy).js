5553639d019d9b44271f527c5bca2266be86c0bc
Talha Habib <prothinker00@hotmail.com>
2018-04-19 11:23:54 +0500

    update

--- app (copy).js
+++ app (copy).js
@@ -1,4 +1,4 @@
-$(function($) {
+(function($) {
 $log                = true;
 $newPosition        = 0;
 $oldChildPosition   = 0;
@@ -10,18 +10,6 @@ $oldIdentity        = 0;
 $oldPosition        = 0;
 var OldCoords       = [];
 var Processed       = 1;
-$markerControl      = false;
-var ii = 0;
-// setInterval(function(){
-//   //console.log("test  "+ii);
-//   console.log(".block[data-id='7']:eq("+ii+")");
-//   $(document).find(".block[data-id='7']:eq("+ii+")").click();
-//   ii++;
-//   if(ii==8){
-//     ii = 0;
-//   }
-//
-// },150);
 // OldCoords["a1"] = [];
 // OldCoords["b1"] = [];
 // OldCoords["c1"] = [];
@@ -79,16 +67,6 @@ $(".EnableNumbering").click(function(){
     $(this).html("On");
   }
 });
-$(".MarkerControl").click(function(){
-  if($markerControl){
-    $markerControl = false;
-    $(this).html("Off");
-  }else{
-    $markerControl = true;
-    $(this).html("On");
-  }
-});
-
 
 // $.each(["addClass","removeClass"],function(i,methodname){
 //       var oldmethod = $.fn[methodname];
@@ -105,16 +83,8 @@ function highlight($line,$position){
   $(".block[data-id='"+$line+"'][data-child='"+$position+"']").addClass("range marked");
 }
 function removeHighlight($id){
-  $(".old_"+$id+"_marker[data-sub]").each(function(){
-    if($(this).attr("data-sub")!=1){
-      $(this).attr("data-sub",parseInt($(this).attr("data-sub")-1));
-    }else{
-      $(this).removeAttr("data-sub");
-    }
-  });
-  $(".old_"+$id+"_marker").not("[data-sub]").removeClass("marker_"+$id+" marker_"+$id+"_top_left \
-   marker_"+$id+"_top_right old_"+$id+"_marker marker_"+$id+"_up marker_"+$id+"_down marker_"+$id+"_bottom_left marker_"+$id+"_bottom_left_right marker_"+$id+"_top marker_"+$id+"_bottom marker_"+$id+"_left marker_"+$id+"_right ");
-
+  $(".marker_"+$id+".old_"+$id+"_marker").removeClass("marker_"+$id+" marker_"+$id+"_top_left \
+   marker_"+$id+"_top_right marker_"+$id+"_bottom_left marker_"+$id+"_bottom_left_right marker_"+$id+"_top marker_"+$id+"_bottom marker_"+$id+"_left marker_"+$id+"_right ");
 }
 $fnOldLine      = 0;
 $fnOldPosition  = 0;
@@ -143,18 +113,15 @@ function ProcessPlacing($oldPosition,$oldLine, $newPosition,$newLine){
   $(".line[data-row='"+$newLine+"'] .block[data-id='"+$newLine+"'][data-child='"+$newPosition+"']").html($(".line[data-row='"+$oldLine+"']").find(".block[data-id='"+$oldLine+"'][data-child='"+$oldPosition+"'] img")[0]).addClass("selected");
   $(".line[data-row='"+$oldLine+"']").find(".block[data-id='"+$oldLine+"'][data-child='"+$oldPosition+"'] img").remove();
   $(".block").removeClass("range");
+
 }
 function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position,$marker=true){
   $identity  = $identity.toLowerCase();
   var line   = $(".line[data-row='"+$parent+"']");
   var block  = $(".block[data-child='"+$child+"'][data-id='"+$parent+"']");
   $id        = block.find("img").attr("data-unique");
+  $(".marker_"+$id).addClass("old_"+$id+"_marker");
 
-  if($markerControl){
-    $marker = false;
-  }else{
-    $marker = $marker;
-  }
   if($marker){
     $ranger = "range"; // <-- highlights the visible move
   }else{
@@ -179,41 +146,19 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
         $newTempLineForPawn = newPawnLine;
         while($i<=$loop){
           if($newTempLineForPawn.find(".block[data-child='"+$child+"'] img").attr("data-id")!=$position || $newTempLineForPawn.find(".block[data-child='"+$child+"'] img").length==0){
-
-              // straight
-              if($newTempLineForPawn.find(".block[data-child='"+$child+"'] img").length==0){
-                if($i==$loop && $marker==false){
-                  $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger+" marker_"+$id+"_"+$direction).attr("data-sub",2);
-                }else{
-                  $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger+" marker_"+$id+"_"+$direction).attr("data-sub",1);
-                }
-              }
-              // side
-              // if the close breaker exist, the marker loop is on down side. otherwise it is up.
-              // right
-              $child = parseInt($child);
-              var rightPawnBlock = $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']");
-              var leftPawnBlock = $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']");
-              if(newPawnLine.attr("data-breakdown")==$position){
-                if($i<$loop || rightPawnBlock.find("img").length==1 || leftPawnBlock.find("img").length==1){
-                  if(rightPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']").addClass($ranger+" marker_"+$id+"_right");
-                  }
-                  if(leftPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']").addClass($ranger+" marker_"+$id+"_left");
-                  }
-                }
-              }else{
-                if($i==$loop || rightPawnBlock.find("img").length==1 || leftPawnBlock.find("img").length==1){
-                  if(rightPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']").addClass($ranger+" marker_"+$id+"_right");
-                  }
-                  if(leftPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']").addClass($ranger+" marker_"+$id+"_left");
-                  }
-                }
-              }
-
+            if($newTempLineForPawn.find(".block[data-child='"+$child+"'] img").length==0){
+              $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger);
+            }
+            if(($newTempLineForPawn.find(".block[data-child='"+parseInt(parseInt($child)+1)+"']").find("img").attr("data-id")!=$position &&
+            $newTempLineForPawn.find(".block[data-child='"+parseInt(parseInt($child)+1)+"']").find("img").length==1 && $i<=1) || $marker!=true)
+            {
+              $newTempLineForPawn.find(".block[data-child='"+parseInt(parseInt($child)+1)+"']").addClass($ranger+" marker_"+$id+"_right");
+            }
+            if(($newTempLineForPawn.find(".block[data-child='"+parseInt(parseInt($child)-1)+"']").find("img").attr("data-id")!=$position &&
+            $newTempLineForPawn.find(".block[data-child='"+parseInt(parseInt($child)-1)+"']").find("img").length==1 && $i<=1) ||  $marker!=true)
+            {
+              $newTempLineForPawn.find(".block[data-child='"+parseInt(parseInt($child)-1)+"']").addClass($ranger+" marker_"+$id+"_left");
+            }
             if($direction=="up"){
                 if($newTempLineForPawn.prev().find(".block[data-child='"+$child+"'] img").attr("data-id")!=$position && $newTempLineForPawn.find(".block[data-child='"+$child+"'] img").length==1){
                   break;
@@ -233,23 +178,6 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
           }
           $i++;
         }
-        if($direction=="down"){
-          if(parseInt(8-$parent)){
-            // white
-            $(".dailoger").removeClass("dailoger");
-            $(".dailog").find("li.queen").html($("img[data-unique='d1']").parent().html());
-            $(".dailog").find("li.rook").html($("img[data-unique='a1']").parent().html());
-            $(".dailog").find("li.bishop").html($("img[data-unique='c1']").parent().html());
-            $(".dailog").find("li.knight").html($("img[data-unique='b1']").parent().html());
-            $("img[data-unique='"+$id+"']").parent().addClass("PawnChanger");
-            console.log($("img[data-unique='"+$id+"']"));
-          }
-        }else{
-          if(parseInt($parent-1)==0){
-            alert("Change black "+$id);
-          }
-        }
-
       }
       //** End Pawn **/
     }
@@ -561,13 +489,10 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
       // end queen
   }
   //** End Queen **/
-  // adding age to marker_
-
   if($marker==false){
+
     removeHighlight($id);
-    $(".marker_"+$id+",.marker_"+$id+"_right,.marker_"+$id+"_left,.marker_"+$id+"_"+$direction).addClass("old_"+$id+"_marker");
   }
-  $(".marker_"+$id+",.marker_"+$id+"_right,.marker_"+$id+"_left,.marker_"+$id+"_"+$direction).addClass("old_"+$id+"_marker");
 }
 // logging
 $(".toggleLogs").click(function(){
--- index.php
+++ index.php
@@ -1,17 +1,76 @@
 <style>
+.dailoger{
+  display:none;
+}
+.dailog{
+  width:50%;
+  height:50%;
+  margin:0 auto;
+  min-width:300px;
+  min-height:300px;
+  border:1px solid grey;
+  padding-right:20px;
+  z-index:2 !important;
+  background:white;
+}
+.overlay-dailog{
+  background:rgba(255,255,255,0.3);
+  width:100%;
+  height:100%;
+  top:0;
+  left:0;
+  position:absolute;
+  z-index:1 !important;
+}
+.relative{
+  z-index:2;
+  left:30%;
+  position:absolute;
+  margin:0 auto;
+  width:40%;
+   z-index:2 !important;
+
+}
+.pawner ul{
+  display:inline-flex;
+  text-align:center;
+  align-item:center;
+  list-style:none;
+}
+.pawner li{
+  width:80px;
+  height:80px;
+  border:1px solid grey;
+}
+.noborder{
+  border:0px none !important;
+}
+.heading{
+  text-align:center;
+  padding:10px;
+  font-size:20px;
+}
+.heading:before,.heading:after{
+  content:"--------";
+}
+.pawner .hoverable:hover{
+  background:grey;
+  cursor:pointer;
+}
 .board{
   margin:0 auto;
-  width:53%;
+  width:750px;
   background:white;
   border:1px solid lightgrey;
   text-align: center;
   align-items: center;
   align-self: center;
   vertical-align: center;
+  position:relative;
 }
-.marker{
-  border:1px dashed grey;
-  background: rgba(0,0,255,0.5) !important;
+*[class*='marker_']{
+  background:rgba(0,0,255,0.7) !important;
+  transition: 90ms;
 }
 .range{
   background:rgba(255,0,0,0.7) !important;
@@ -101,59 +160,105 @@
   overflow: auto;
   padding:10px;
 }
+.bottom-scaler{
+  list-style: none;
+  float:left;
+  position: absolute;
+  left:-100px;
+}
+.bottom-scaler li{
+  padding:7px;
+  width:10px;
+  height:80px;
+}
+.left-scaler{
+  list-style: none;
+  display:inline-flex;
+  width:100%;
+  position: absolute;
+  left:-35px;
+  bottom:-800px;
+}
+.left-scaler li{
+  padding:7px;
+  width:80px;
+  height:10px;
+
+}
+
 </style>
 
 <div class="board" align="center">
+  <ul class="left-scaler">
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+    <li>D</li>
+    <li>E</li>
+    <li>F</li>
+    <li>G</li>
+    <li>H</li>
+  </ul>
+  <ul class="bottom-scaler">
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+  </ul>
   <div class="line" data-row="1" data-type="container" data-starting="white">
     <div class="block" data-child="1" data-id="1" data-type="block" data-dir="down">
-      <img src="./image/rookw.png" data-type="rook" data-color="white" data-dir="down" data-id="opponent">
+      <img src="./image/rookw.png" data-type="rook" data-unique="a1" data-color="white" data-dir="down" data-id="opponent">
     </div>
     <div class="block" data-child="2" data-id="1" data-color="white" data-dir="down" data-type="block">
-      <img src="./image/knightw.png" data-type="knight" data-color="white" data-dir="down" data-id="opponent">
+      <img src="./image/knightw.png" data-type="knight" data-unique="b1" data-color="white" data-dir="down" data-id="opponent">
     </div>
     <div class="block" data-child="3" data-id="1" data-color="white" data-dir="down" data-type="block">
-      <img src="./image/bishopw.png" data-type="bishop" data-color="white" data-dir="down" width="90%" align="center" data-id="opponent">
+      <img src="./image/bishopw.png" data-type="bishop" data-unique="c1" data-color="white" data-dir="down" width="90%" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="4" data-id="1" data-type="block">
-      <img src="./image/queenw.png" data-type="queen" data-color="white" data-dir="down"  width="95%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/queenw.png" data-type="queen" data-unique="d1" data-color="white" data-dir="down"  width="95%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="5" data-id="1" data-type="block">
-      <img src="./image/kingw.png" data-type="king" data-color="white" data-dir="down" width="90%" align="center"  data-id="opponent">
+      <img src="./image/kingw.png" data-type="king" data-unique="e1" data-color="white" data-dir="down" width="90%" align="center"  data-id="opponent">
     </div>
     <div class="block" data-child="6" data-id="1" data-type="block">
-      <img src="./image/bishopw.png" data-type="bishop" data-color="white" data-dir="down" width="90%" align="center" data-id="opponent">
+      <img src="./image/bishopw.png" data-type="bishop" data-unique="f1" data-color="white" data-dir="down" width="90%" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="7" data-id="1" data-type="block">
-      <img src="./image/knightw.png" data-type="knight" data-color="white" data-dir="down" data-id="opponent">
+      <img src="./image/knightw.png" data-type="knight" data-unique="g1" data-color="white" data-dir="down" data-id="opponent">
     </div>
     <div class="block" data-child="8" data-id="1" data-type="block">
-      <img src="./image/rookw.png" data-type="rook" data-color="white" data-dir="down" data-id="opponent">
+      <img src="./image/rookw.png" data-type="rook" data-unique="h1" data-color="white" data-dir="down" data-id="opponent">
     </div>
   </div>
   <div class="line" data-row="2" data-type="container" data-starting="white">
     <div class="block" data-child="1" data-id="2" data-type="block" data-dir="down">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="a2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="2" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="b2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="3" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="c2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="4" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="d2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="5" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="e2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="6" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="f2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="7" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="g2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
     <div class="block" data-child="8" data-id="2" data-type="block">
-      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" width="60%" style="margin-top:10px" align="center" data-id="opponent">
+      <img src="./image/pawnw.png" data-type="pawn" data-color="white" data-dir="down" data-unique="h2" width="60%" style="margin-top:10px" align="center" data-id="opponent">
     </div>
   </div>
   <div class="line" data-row="3" data-type="container" data-breakdown="opponent" data-id="opponent">
@@ -198,61 +303,82 @@
   </div>
   <div class="line" data-row="7" data-type="container" data-starting="black" data-dir="up">
     <div class="block" data-child="1" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="a7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="2" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="b7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="3" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="c7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="4" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="d7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="5" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="e7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="6" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="f7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="7" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="g7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="8" data-id="7" data-type="block">
-      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" width="60%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/pawnb.png" data-type="pawn" data-color="black" data-dir="up" data-unique="h7" width="60%" style="margin-top:10px" align="center" data-id="self">
     </div>
   </div>
   <div class="line" data-row="8" data-type="container" data-starting="black">
     <div class="block" data-child="1" data-id="8" data-type="block">
-      <img src="./image/rookb.png" data-type="rook" data-color="black" data-dir="up" data-id="self">
+      <img src="./image/rookb.png" data-type="rook" data-color="black" data-dir="up" data-unique="a8" data-id="self">
     </div>
     <div class="block" data-child="2" data-id="8" data-type="block">
-      <img src="./image/knightb.png" data-type="knight" data-color="black" data-dir="up" data-id="self">
+      <img src="./image/knightb.png" data-type="knight" data-color="black" data-dir="up" data-unique="b8" data-id="self">
     </div>
     <div class="block" data-child="3" data-id="8" data-type="block">
-      <img src="./image/bishopb.png" data-type="bishop" data-color="black" data-dir="up" width="90%" align="center" data-id="self">
+      <img src="./image/bishopb.png" data-type="bishop" data-color="black" data-dir="up" data-unique="c8" width="90%" align="center" data-id="self">
     </div>
     <div class="block" data-child="4" data-id="8" data-type="block">
-      <img src="./image/queenb.png" data-type="queen" data-color="black" data-dir="up"  width="95%" style="margin-top:10px" align="center" data-id="self">
+      <img src="./image/queenb.png" data-type="queen" data-color="black" data-dir="up" data-unique="d8"  width="95%" style="margin-top:10px" align="center" data-id="self">
     </div>
     <div class="block" data-child="5" data-id="8" data-type="block">
-      <img src="./image/kingb.png" data-type="king" data-color="black" data-dir="up" width="90%" align="center"  data-id="self">
+      <img src="./image/kingb.png" data-type="king" data-color="black" data-dir="up" data-unique="e8" width="90%" align="center"  data-id="self">
     </div>
     <div class="block" data-child="6" data-id="8" data-type="block">
-      <img src="./image/bishopb.png" data-type="bishop" data-color="black" data-dir="up" width="90%" align="center" data-id="self">
+      <img src="./image/bishopb.png" data-type="bishop" data-color="black" data-dir="up" data-unique="f8" width="90%" align="center" data-id="self">
     </div>
     <div class="block" data-child="7" data-id="8" data-type="block">
-      <img src="./image/knightb.png" data-type="knight" data-color="black" data-dir="up" data-id="self">
+      <img src="./image/knightb.png" data-type="knight" data-color="black" data-unique="g8" data-dir="up" data-id="self">
     </div>
     <div class="block" data-child="8" data-id="8" data-type="block">
-      <img src="./image/rookb.png" data-type="rook" data-color="black" data-dir="up" data-id="self">
+      <img src="./image/rookb.png" data-type="rook" data-color="black" data-unique="h8" data-dir="up" data-id="self">
     </div>
   </div>
 </div>
+<div class="relative dailoger">
+<div class="dailog">
+  <div class="heading">
+    Choose your pawn
+  </div>
+  <div class="pawner">
+    <ul>
+      <li class="noborder"></li>
+      <li class="hoverable queen"></li>
+    </ul>
+    <ul>
+      <li class="hoverable rook"></li>
+      <li class="hoverable bishop"></li>
+      <li class="hoverable knight"></li>
+    </ul>
+  </div>
+</div>
+</div>
+<div class="overlay-dailog dailoger">
+ </div>
 <div class="logs">
   Turn the log <a href="javascript:void(0);" class="toggleLogs">Off</a><br>
   <a href="javascript:void(0);" class="ClearLog">Clear Log</a><br>
   Free Move Mode is <a href="javascript:void(0);" class="FreeMoveMode">Off</a><br>
+  Marker is <a href="javascript:void(0);" class="MarkerControl">Off</a><br>
 </div>
 <div class="status"></div>
 <div class="rendered"></div>
