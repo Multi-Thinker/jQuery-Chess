e13c72d88ddaab677fdbe632aeb9c824cc9f6a07
Talha Habib <prothinker00@hotmail.com>
2018-04-30 12:57:20 +0500

    update

--- app.js
+++ app.js
@@ -11,49 +11,15 @@ $oldPosition        = 0;
 var OldCoords       = [];
 var Processed       = 1;
 $markerControl      = false;
-var ii = 0;
-// setInterval(function(){
-//   //console.log("test  "+ii);
-//   console.log(".block[data-id='7']:eq("+ii+")");
-//   $(document).find(".block[data-id='7']:eq("+ii+")").click();
-//   ii++;
-//   if(ii==8){
-//     ii = 0;
-//   }
-//
-// },150);
-// OldCoords["a1"] = [];
-// OldCoords["b1"] = [];
-// OldCoords["c1"] = [];
-// OldCoords["d1"] = [];
-// OldCoords["e1"] = [];
-// OldCoords["f1"] = [];
-// OldCoords["g1"] = [];
-// OldCoords["h1"] = [];
-// OldCoords["a2"] = [];
-// OldCoords["b2"] = [];
-// OldCoords["c2"] = [];
-// OldCoords["d2"] = [];
-// OldCoords["e2"] = [];
-// OldCoords["f2"] = [];
-// OldCoords["g2"] = [];
-// OldCoords["h2"] = [];
-// OldCoords["a7"] = [];
-// OldCoords["b7"] = [];
-// OldCoords["c7"] = [];
-// OldCoords["d7"] = [];
-// OldCoords["e7"] = [];
-// OldCoords["f7"] = [];
-// OldCoords["g7"] = [];
-// OldCoords["h7"] = [];
-// OldCoords["a8"] = [];
-// OldCoords["b8"] = [];
-// OldCoords["c8"] = [];
-// OldCoords["d8"] = [];
-// OldCoords["e8"] = [];
-// OldCoords["f8"] = [];
-// OldCoords["g8"] = [];
-// OldCoords["h8"] = [];
+$whiteQueen         = $("img[data-unique='d1']").parent().html();
+$whiteRook          = $("img[data-unique='a1']").parent().html();
+$whiteBishop        = $("img[data-unique='c1']").parent().html();
+$whiteKnight        = $("img[data-unique='b1']").parent().html();
+$blackQueen          = $("img[data-unique='d8']").parent().html();
+$blackRook           = $("img[data-unique='a8']").parent().html();
+$blackBishop         = $("img[data-unique='c8']").parent().html();
+$blackKnight         = $("img[data-unique='b8']").parent().html();
+
 
 // logging
 function status($str,$force=false){
@@ -89,7 +55,6 @@ $(".MarkerControl").click(function(){
   }
 });
 
-
 // $.each(["addClass","removeClass"],function(i,methodname){
 //       var oldmethod = $.fn[methodname];
 //       $.fn[methodname] = function(){
@@ -113,7 +78,9 @@ function removeHighlight($id){
     }
   });
   $(".old_"+$id+"_marker").not("[data-sub]").removeClass("marker_"+$id+" marker_"+$id+"_top_left \
-   marker_"+$id+"_top_right old_"+$id+"_marker marker_"+$id+"_up marker_"+$id+"_down marker_"+$id+"_bottom_left marker_"+$id+"_bottom_left_right marker_"+$id+"_top marker_"+$id+"_bottom marker_"+$id+"_left marker_"+$id+"_right ");
+   marker_"+$id+"_top_right old_"+$id+"_marker marker_"+$id+"_up marker_"+$id+"_down \
+   marker_"+$id+"_bottom_left marker_"+$id+"_bottom_right marker_"+$id+"_top marker_"+$id+"_bottom \
+   marker_"+$id+"_left marker_"+$id+"_right ");
 
 }
 $fnOldLine      = 0;
@@ -158,6 +125,7 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
   if($marker){
     $ranger = "range"; // <-- highlights the visible move
   }else{
+      removeHighlight($id);
       $ranger = "marker_"+$id;
   }
     if(line.prev().length>0){
@@ -183,9 +151,9 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
               // straight
               if($newTempLineForPawn.find(".block[data-child='"+$child+"'] img").length==0){
                 if($i==$loop && $marker==false){
-                  $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger+" marker_"+$id+"_"+$direction).attr("data-sub",2);
+                  $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger+" marker_"+$id+"_"+$direction+" old_"+$id+"_marker").attr("data-sub",2);
                 }else{
-                  $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger+" marker_"+$id+"_"+$direction).attr("data-sub",1);
+                  $newTempLineForPawn.find(".block[data-child='"+$child+"']").addClass($ranger+" marker_"+$id+"_"+$direction+" old_"+$id+"_marker").attr("data-sub",1);
                 }
               }
               // side
@@ -194,22 +162,23 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
               $child = parseInt($child);
               var rightPawnBlock = $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']");
               var leftPawnBlock = $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']");
+
               if(newPawnLine.attr("data-breakdown")==$position){
-                if($i<$loop || rightPawnBlock.find("img").length==1 || leftPawnBlock.find("img").length==1){
+                if($i<$loop){ // || rightPawnBlock.find("img").length==1 || leftPawnBlock.find("img").length==1
                   if(rightPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']").addClass($ranger+" marker_"+$id+"_right");
+                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']").addClass($ranger+" marker_"+$id+"_right"+" old_"+$id+"_marker");
                   }
                   if(leftPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']").addClass($ranger+" marker_"+$id+"_left");
+                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']").addClass($ranger+" marker_"+$id+"_left "+" old_"+$id+"_marker");
                   }
                 }
               }else{
                 if($i==$loop || rightPawnBlock.find("img").length==1 || leftPawnBlock.find("img").length==1){
                   if(rightPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']").addClass($ranger+" marker_"+$id+"_right");
+                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child+1)+"']").addClass($ranger+" marker_"+$id+"_right "+" old_"+$id+"_marker");
                   }
                   if(leftPawnBlock.find("img").length==1 || $marker==false){
-                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']").addClass($ranger+" marker_"+$id+"_left");
+                    $newTempLineForPawn.find(".block[data-child='"+parseInt($child-1)+"']").addClass($ranger+" marker_"+$id+"_left "+" old_"+$id+"_marker");
                   }
                 }
               }
@@ -234,19 +203,27 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
           $i++;
         }
         if($direction=="down"){
-          if(parseInt(8-$parent)){
+          if(parseInt(8-$parent)==0){
             // white
-            $(".dailoger").removeClass("dailoger");
-            $(".dailog").find("li.queen").html($("img[data-unique='d1']").parent().html());
-            $(".dailog").find("li.rook").html($("img[data-unique='a1']").parent().html());
-            $(".dailog").find("li.bishop").html($("img[data-unique='c1']").parent().html());
-            $(".dailog").find("li.knight").html($("img[data-unique='b1']").parent().html());
-            $("img[data-unique='"+$id+"']").parent().addClass("PawnChanger");
-            console.log($("img[data-unique='"+$id+"']"));
+            if($("img[data-unique='"+$id+"']").parent().hasClass("PawnChanged")==false){
+              $(".dailoger").removeClass("dailoger");
+              $(".dailog").find("li.queen").html($whiteQueen);
+              $(".dailog").find("li.rook").html($whiteRook);
+              $(".dailog").find("li.bishop").html($whiteBishop);
+              $(".dailog").find("li.knight").html($whiteKnight);
+              $("img[data-unique='"+$id+"']").parent().addClass("PawnChanger");
+            }
           }
         }else{
           if(parseInt($parent-1)==0){
-            alert("Change black "+$id);
+            if($("img[data-unique='"+$id+"']").parent().hasClass("PawnChanged")==false){
+              $(".dailoger").removeClass("dailoger");
+              $(".dailog").find("li.queen").html($blackQueen);
+              $(".dailog").find("li.rook").html($blackRook);
+              $(".dailog").find("li.bishop").html($blackBishop);
+              $(".dailog").find("li.knight").html($blackKnight);
+              $("img[data-unique='"+$id+"']").parent().addClass("PawnChanger");
+            }
           }
         }
 
@@ -259,13 +236,13 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
     var linesOnTop  = line.prevAll().length;
     var linesOnBottom  = line.nextAll().length;
     $parent = parseInt($parent);
-    // Line top Left
+    // Line top right
     $TopLeftchild = parseInt($child);
-    for(i=linesOnTop;i>=1;i--){
+    for(i=parseInt($parent)-1;i>=1;i--){
         $TopLeftchild = $TopLeftchild + 1;
         var tempBishopLine = $(".line[data-row='"+i+"'] .block[data-child='"+$TopLeftchild+"']");
         if(tempBishopLine.find("img").attr("data-id")!=$position || tempBishopLine.find("img").length==0){
-          tempBishopLine.addClass($ranger+" marker_"+$id+"_top_left");
+          tempBishopLine.addClass($ranger+" marker_"+$id+"_top_right old_"+$id+"_marker");
           if(tempBishopLine.find("img").attr("data-id")!=$position && tempBishopLine.find("img").length==1){
             break;
           }
@@ -273,13 +250,14 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
           break;
         }
     }
-    //Line top Right
+    //Line top Left
     $TopRightchild = parseInt($child);
-    for(i=linesOnTop;i>=1;i--){
+    for(i=parseInt($parent)-1;i>=1;i--){
         $TopRightchild = $TopRightchild - 1;
         var tempBishopLine = $(".line[data-row='"+i+"'] .block[data-child='"+$TopRightchild+"']");
+        console.log(tempBishopLine);
         if(tempBishopLine.find("img").attr("data-id")!=$position || tempBishopLine.find("img").length==0){
-          tempBishopLine.addClass($ranger+" marker_"+$id+"_top_right");
+          tempBishopLine.addClass($ranger+" marker_"+$id+"_top_left old_"+$id+"_marker");
           if(tempBishopLine.find("img").attr("data-id")!=$position && tempBishopLine.find("img").length==1){
             break;
           }
@@ -299,7 +277,7 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
           // console.log(tempBishopLine);
           if(tempBishopLine.find("img").attr("data-id")!=$position || tempBishopLine.find("img").length==0){
             // console.log($ranger+":::"+x+" adding");
-            tempBishopLine.addClass($ranger+" marker_"+$id+"_bottom_left");
+            tempBishopLine.addClass($ranger+" marker_"+$id+"_bottom_left old_"+$id+"_marker");
             if(tempBishopLine.find("img").attr("data-id")!=$position && tempBishopLine.find("img").length==1){
               break;
             }
@@ -315,7 +293,7 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
         if(i!=$parent){
           var tempBishopLine =  $(".line[data-row='"+i+"'] .block[data-child='"+$BottomRightchild+"']");
           if(tempBishopLine.find("img").attr("data-id")!=$position || tempBishopLine.find("img").length==0){
-            tempBishopLine.addClass($ranger+" marker_"+$id+"_bottom_right");
+            tempBishopLine.addClass($ranger+" marker_"+$id+"_bottom_right old_"+$id+"_marker");
             if(tempBishopLine.find("img").attr("data-id")!=$position && tempBishopLine.find("img").length==1){
               break;
             }
@@ -334,8 +312,8 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
    //console.log($ranger);
    while(loop<=2){          //<-- loop for line above
       var knightParent = knightLine.next().attr("data-row");
-      knightLine.prev().setChild([parseInt($child)+x,$position,$ranger+" marker_"+$id+"_top_right",knightLine.prev().attr("data-row")]); // 0 0 0 0 X
-      knightLine.prev().setChild([parseInt($child)-x,$position,$ranger+" marker_"+$id+"_top_left",knightLine.prev().attr("data-row")]); // X 0 0 0 X
+      knightLine.prev().setChild([parseInt($child)+x,$position,$ranger+" marker_"+$id+"_top_right marker_"+$id+"_up old_"+$id+"_marker",knightLine.prev().attr("data-row")]); // 0 0 0 0 X
+      knightLine.prev().setChild([parseInt($child)-x,$position,$ranger+" marker_"+$id+"_top_left marker_"+$id+"_up old_"+$id+"_marker",knightLine.prev().attr("data-row")]); // X 0 0 0 X
       knightLine = knightLine.prev();
       x = x-1;
       loop=loop+1;
@@ -345,8 +323,8 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
    var loop = 1;
    while(loop<=2){          //<-- loop for line below
       var knightParent = knightLine.next().attr("data-row");
-      knightLine.next().setChild([parseInt($child)+x,$position,$ranger+" marker_"+$id+"_bottom_right",knightLine.next().attr("data-row")]); // 0 0 0 X 0
-      knightLine.next().setChild([parseInt($child)-x,$position,$ranger+" marker_"+$id+"_bottom_left",knightLine.next().attr("data-row")]); // 0 X 0 X 0
+      knightLine.next().setChild([parseInt($child)+x,$position,$ranger+" marker_"+$id+"_bottom_right marker_"+$id+"_down old_"+$id+"_marker",knightLine.next().attr("data-row")]); // 0 0 0 X 0
+      knightLine.next().setChild([parseInt($child)-x,$position,$ranger+" marker_"+$id+"_bottom_left marker_"+$id+"_down old_"+$id+"_marker",knightLine.next().attr("data-row")]); // 0 X 0 X 0
       knightLine = line.next();
       x = x-1;
       loop=loop+1;
@@ -375,24 +353,25 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
   if($identity=="rook"){
       var rookLineTop   = line;
       //Line Down for rook
-      for(var j=1;j<=rookLineTop.nextAll().length;j++){
+      for(var j=1;j<=parseInt($parent);j++){
         var tempLineDown = $(".line[data-row='"+ parseInt(parseInt($parent)+parseInt(j)) +"'] .block[data-child='"+$child+"']");
         if(tempLineDown.find("img").attr("data-id")==$position){
           break;
         }else{
-          tempLineDown.addClass($ranger);
+          tempLineDown.addClass($ranger+" marker_"+$id+"_bottom old_"+$id+"_marker");
           if(tempLineDown.find("img").attr("data-id")!=$position && tempLineDown.find("img").length==1){
             break;
           }
         }
       }
       //Line Top For Rook
-      for(var i=rookLineTop.prevAll().length;i>=1;i--){
+      console.log(rookLineTop.prevAll().length);
+      for(var i=parseInt($parent)-1;i>=1;i--){
         var tempLine = $(".line[data-row='"+i+"'] .block[data-child='"+$child+"']");
         if(tempLine.find("img").attr("data-id")==$position){
           break;
         }else{
-          tempLine.addClass($ranger);
+          tempLine.addClass($ranger+" marker_"+$id+"_up old_"+$id+"_marker");
           if(tempLine.find("img").attr("data-id")!=$position && tempLine.find("img").length==1){
             break;
           }
@@ -404,7 +383,7 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
         if(tempLineLeft.find("img").attr("data-id")==$position){
           break;
         }else{
-          tempLineLeft.addClass($ranger);
+          tempLineLeft.addClass($ranger+ " marker_"+$id+"_left old_"+$id+"_marker");
           if(tempLineLeft.find("img").attr("data-id")!=$position && tempLineLeft.find("img").length==1){
             break;
           }
@@ -418,7 +397,7 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
         if(tempLineRight.find("img").attr("data-id")==$position){
           break;
         }else{
-          tempLineRight.addClass($ranger);
+          tempLineRight.addClass($ranger+" marker_"+$id+"_right old_"+$id+"_marker");
           if(tempLineRight.find("img").attr("data-id")!=$position && tempLineRight.find("img").length==1){
             break;
           }
@@ -564,10 +543,10 @@ function ProcessRange($parent,$child,$line,$direction,$color,$identity,$position
   // adding age to marker_
 
   if($marker==false){
-    removeHighlight($id);
-    $(".marker_"+$id+",.marker_"+$id+"_right,.marker_"+$id+"_left,.marker_"+$id+"_"+$direction).addClass("old_"+$id+"_marker");
+  //  removeHighlight($id);
+    //$(".marker_"+$id+",.marker_"+$id+"_right,.marker_"+$id+"_left,.marker_"+$id+"_"+$direction).addClass("old_"+$id+"_marker");
   }
-  $(".marker_"+$id+",.marker_"+$id+"_right,.marker_"+$id+"_left,.marker_"+$id+"_"+$direction).addClass("old_"+$id+"_marker");
+  //$(".marker_"+$id+",.marker_"+$id+"_right,.marker_"+$id+"_left,.marker_"+$id+"_"+$direction).addClass("old_"+$id+"_marker");
 }
 // logging
 $(".toggleLogs").click(function(){
@@ -591,6 +570,14 @@ $(".FreeMoveMode").click(function(){
     $(this).html("On");
   }
 })
+
+//Pawn Changer
+
+$(".hoverable").click(function(){
+  $(".PawnChanger").html($(this).html()).removeClass("PawnChanger").addClass("PawnChanged");
+  $(".dailog").parent().addClass("dailoger");
+  $(".overlay-dailog").addClass("dailoger");
+});
 // block positioning
 $(".block").click(function(){
   $parent   = $(this).attr("data-id");
